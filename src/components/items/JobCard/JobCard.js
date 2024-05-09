78eb688abf8323b728d3ed7f2d4f5d4a3377f256
Sahil Amin <sahilamin68@gmail.com>
2024-05-09 22:25:24 +0530

    static data mapped and infinite scroll implemented

--- src/components/items/JobCard/JobCard.js
+++ src/components/items/JobCard/JobCard.js
@@ -1,10 +1,22 @@
 import React, { useState } from 'react';
-import { Card, CardContent, Typography, Button, CardActions, Chip, Avatar, Box} from '@mui/material';
+import { Card, CardContent, Typography, Button, CardActions, Chip, Avatar, Box } from '@mui/material';
 import styles from './jobCard.module.css';
 import { Link } from 'react-router-dom';
 
-const JobCard = () => {
-  const [isVisible, setIsVisible] = useState(false)
+const JobCard = ({
+  jdLink,
+  jobDetailsFromCompany,
+  maxJdSalary,
+  minJdSalary,
+  salaryCurrencyCode,
+  location,
+  minExp,
+  maxExp,
+  jobRole,
+  companyName,
+  logoUrl
+}) => {
+  const [isVisible, setIsVisible] = useState(false);
 
   return (
     <Card className={styles.cardBody}>
@@ -12,38 +24,41 @@ const JobCard = () => {
         <Chip 
           className={styles.chip} 
           sx={{
-            '&.MuiChip-root':{
+            '&.MuiChip-root': {
               height: 'fit-content'
             }
           }} 
-          label="⏳ Posted 10 days ago"/>
+          label="⏳ Posted 10 days ago"
+        />
       </Box>
       <CardContent className={styles.cardContent}>
         <Box display='flex' gap={1.2}>
           <img
             className={styles.logo} 
-            src="https://storage.googleapis.com/weekday-assets/airtableAttachment_1713598325603_7ico7.jpg" 
-            alt="company_image"/>
+            src={logoUrl} 
+            alt="company_image"
+          />
           <Box>
             <Typography variant='h3' className={styles.title}>
-              fampay
+              {companyName}
             </Typography>
             <Typography variant='h2' className={styles.subTitle}>
-              Backend Engineer
+              {jobRole}
             </Typography>
             <Typography className={styles.location}>
-              Bangalore
+              {location}
             </Typography>
           </Box>
         </Box>  
         <Typography variant='body2' className={styles.salary}>
-          Estimated Salary: ₹18 - 35 LPA ✅
+          Estimated Salary: {salaryCurrencyCode} {minJdSalary} {minJdSalary != null && maxJdSalary != null && '-'} {maxJdSalary} LPA ✅
         </Typography>
         <Box 
           className={styles.aboutContainer} 
           sx={{
-            maskImage: !isVisible?'linear-gradient(rgb(255, 255, 255), rgb(255, 255, 255), rgba(255, 255, 255, 0)) !important':'unset',
-          }}>
+            maskImage: !isVisible ? 'linear-gradient(rgb(255, 255, 255), rgb(255, 255, 255), rgba(255, 255, 255, 0)) !important' : 'unset',
+          }}
+        >
           <Box>
             <Typography variant="body1" fontWeight={500}>
               About Company:
@@ -51,16 +66,18 @@ const JobCard = () => {
             <Box 
               fontSize='14px' 
               whiteSpace='pre-wrap' 
-              padding='0 20px 0 0'>
+              padding='0 20px 0 0'
+            >
               <p><strong>About Us</strong></p>
               <p 
                 className={styles.aboutDescription}
                 style={{
-                    height: !isVisible?'auto':'167px',
-                    overflow: !isVisible?'hidden':'auto'
-                }}>
+                  height: !isVisible ? 'auto' : '167px',
+                  overflow: !isVisible ? 'hidden' : 'auto'
+                }}
+              >
                 <span style={{fontWeight: '400'}}>
-                This is a sample job and you must have displayed it to understand that its not just some random lorem ipsum text but something which was manually written. Oh well, if random text is what you were looking for then here it is: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages and now in this assignment.
+                  {jobDetailsFromCompany}
                 </span>
               </p>
             </Box>
@@ -69,33 +86,44 @@ const JobCard = () => {
         <Box 
           className={styles.viewButtonContainer} 
           sx={{
-            top: !isVisible?'-27px':'0px',
-            marginTop: !isVisible?'0':'20px'
-          }}>
+            top: !isVisible ? '-27px' : '0px',
+            marginTop: !isVisible ? '0' : '20px'
+          }}
+        >
           <div className={styles.viewButton} onClick={() => setIsVisible(!isVisible)}>
             {
-              isVisible? 'Hide job': 'View job'
+              isVisible ? 'Hide job' : 'View job'
             }
           </div>
         </Box>
         <Box>
-            <Typography variant='h3' className={styles.title}>
+          <Typography variant='h3' className={styles.title}>
             Minimum Experience:
-            </Typography>
-            <Typography variant='h2' className={styles.subTitle}>2 years</Typography>
+          </Typography>
+          <Typography variant='h2' className={styles.subTitle}>
+            {minExp !== null? minExp: 0} years
+          </Typography>
         </Box>
       </CardContent>
       <Box className={styles.cardActions}>
         <Button className={styles.cardActionsApplyButton} variant="contained">
-        ⚡ Easy Apply
+          ⚡ Easy Apply
         </Button>
-        {/* <Button className={styles.cardActionsReferralButton} variant="contained">
+        <Button className={styles.cardActionsReferralButton} variant="contained">
+          <Box display='flex' gap={.7} mr={1.2}>
             <Avatar 
-                alt="Remy Sharp" 
-                src="/static/images/avatar/1.jpg" 
-                className={styles.cardActionsButtonAvatar}/> 
-            <span className={styles.cardActionsButtoSpan}>Unlock referral asks</span>    
-        </Button> */}
+              alt="Company Logo" 
+              src={'https://blog.photofeeler.com/wp-content/uploads/2017/12/linkedin-profile-picture.jpg'} 
+              className={styles.cardActionsButtonAvatar}
+            /> 
+            <Avatar 
+              alt="Company Logo" 
+              src={'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'} 
+              className={styles.cardActionsButtonAvatar}
+            /> 
+          </Box>
+          <span className={styles.cardActionsButtonSpan}>Unlock referral asks</span>    
+        </Button>  
       </Box>
     </Card>
   );
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,20 +1,84 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import JobCard from '../../components/items/JobCard/JobCard';
-import { Grid } from '@mui/material';
+import { Grid, CircularProgress } from '@mui/material';
 import styles from './home.module.css';
+import { getSampleJdJSON } from '../data';
 
 const Home = () => {
+  const [jobData, setJobData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [startIndex, setStartIndex] = useState(0);
+  const itemsPerPage = 9;
+  const [hasMore, setHasMore] = useState(true)
+
+  const fetchJobData = () => {
+    if (loading || !hasMore) return;
+    setLoading(true);
+    setTimeout(() => {
+      const newDataSlice = getSampleJdJSON().slice(startIndex, startIndex + itemsPerPage);
+
+      if (newDataSlice.length > 0) {
+        setJobData(prevData => [...prevData, ...newDataSlice]);
+        setStartIndex(prevIndex => prevIndex + itemsPerPage);
+      } else {
+        setHasMore(false);
+      }
+
+      setLoading(false);
+    }, 1000);
+  };
+
+  useEffect(() => {
+    const initialDataSlice = getSampleJdJSON().slice(0, itemsPerPage);
+    setJobData(initialDataSlice);
+    setStartIndex(itemsPerPage);
+  }, []);
+
+  const handleScroll = () => {
+    const scrollHeight = window.innerHeight + document.documentElement.scrollTop;
+    const totalHeight = document.documentElement.offsetHeight;
+    const tolerance = 1;
+    if (Math.abs(scrollHeight - totalHeight) <= tolerance) {
+      fetchJobData();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [startIndex, loading, hasMore]);
+
   return (
     <>
-      <Grid container spacing={4} sx={{padding: '24px'}}>
-        {Array.from({ length: 10 }, (_, index) => (
-          <Grid item xs={12} md={4} key={index} className={styles.gridItem}>
-            <JobCard/>
+      <Grid container spacing={4} sx={{ padding: '24px' }}>
+        {jobData.map((item, index) => (
+          <Grid item xs={12} sm={6} md={4} lg={4} xl={4} key={index} className={styles.gridItem}>
+            <JobCard
+              jdLink={item?.jdLink}
+              jobDetailsFromCompany={item?.jobDetailsFromCompany}
+              maxJdSalary={item?.maxJdSalary}
+              minJdSalary={item?.minJdSalary}
+              salaryCurrencyCode={item?.salaryCurrencyCode}
+              location={item?.location}
+              minExp={item?.minExp}
+              maxExp={item?.maxExp}
+              jobRole={item?.jobRole}
+              companyName={item?.companyName}
+              logoUrl={item?.logoUrl}
+            />
           </Grid>
         ))}
       </Grid>
+      {loading && (
+        <div className={styles.loaderContainer}>
+          <CircularProgress />
+        </div>
+      )}
+      {!hasMore && <p>No more jobs to load.</p>}
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
